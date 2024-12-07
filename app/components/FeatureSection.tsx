cc0312079f75c216e61734d08e94fc1581eef2df
anjulk <anjulkumar2001@gmail.com>
2024-12-07 16:00:27 -0500

    Feature section

---
+++ app/components/FeatureSection.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
+
+
+const FeatureSection = () => {
+  const features = [
+    {
+      name: 'Innovative AI Solutions',
+      description:
+        'Solving complex challenges with Data Engineering, Business Intelligence, and GenAI',
+      icon: CloudArrowUpIcon,
+    },
+    {
+      name: 'Confidence and Clarity',
+      description: 'Defining success, eliminating uncertainty, and delivering impactful outcomes',
+      icon: LockClosedIcon,
+    },
+    {
+      name: 'Future-Focused Excellence',
+      description: 'Driven by ownership, expertise, and bold, innovative approaches to create value',
+      icon: ServerIcon,
+    },
+  ]
+
+  return (
+    <div className="overflow-hidden bg-white py-24 sm:py-32">
+    <div className="mx-auto max-w-7xl px-6 lg:px-8">
+      <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+        <div className="lg:pr-8 lg:pt-4">
+          <div className="lg:max-w-lg">
+          <h2 className="text-4xl  font-semibold text-indigo-600">Next-Gen Intelligent Solutions</h2>
+              <p className="mt-2 text-pretty text-2xl font-semibold tracking-tight text-gray-900 sm:text-2xl">
+                Solve Complex Challenges with Confidence
+              </p>
+              <p className="mt-2 text-lg/8 text-gray-600">
+                Drive business growth by leveraging Data Engineering, Business Intelligence, and GenAI to eliminate uncertainty and deliver impactful results.
+              </p>
+            <dl className="mt-4 max-w-xl space-y-4 text-base/7 text-gray-600 lg:max-w-none">
+              {features.map((feature) => (
+                <div key={feature.name} className="relative pl-9">
+                  <dt className="inline font-semibold text-gray-900">
+                    <feature.icon aria-hidden="true" className="absolute left-1 top-1 size-5 text-indigo-600" />
+                    {feature.name}
+                  </dt>{' '}
+                  <dd className="inline">{feature.description}</dd>
+                </div>
+              ))}
+            </dl>
+          </div>
+        </div>
+        <img
+          alt="Product screenshot"
+          src="/aiblock.jpeg"
+          width={720}
+          height={1200}
+          className="w-[48rem] max-w-720 rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+        />
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default FeatureSection
--- app/components/Midsection.tsx
+++ app/components/Midsection.tsx
@@ -6,6 +6,7 @@ import Footer from './Footer';
 import NewsLetterSection from './NewsLetterSection';
 import Testimonial from './Testimonial';
 import Services from './Services';
+import FeatureSection from './FeatureSection';
 
 const Midsection = () => {
   return (
@@ -25,44 +26,8 @@ const Midsection = () => {
       </ul>
 
       {/* ,,,,,, */}
-      <div className="flex bg-gray-700 ">
 
-        <div className='flex-1 mt-10 ml-10'>
-
-          <h1 className='text-orange-600'>WHAT WE DO</h1>
-          <h2 className='text-4xl font-bold text-white mr-5 mt-5'>We deliver confidence by</h2>
-          <h2 className='text-4xl font-bold text-white mr-5 mt-2'> resolving your most </h2>
-          <h2 className='text-4xl font-bold text-white mr-5 mt-2'> challenging problems</h2>
-
-          <p className="mt-2 text-white mt-5 text-base">Effective problem-solving isn’t solely about arriving at answers; it’s about persistently inquiring until we understand what defines success and how to attain it. Our mission is to assist you in steering the optimal course of action, eliminating uncertainty, and advancing with confidence by uniting the strengths of Data Engineering, Business Intellingence and Gen AI .
-          </p>
-          <div>
-
-            <h1 className='text-3xl mt-40 text-orange-600'>What powers us?</h1>
-
-            <h2 className='text-white mt-5 text-base mb-10'>An attribute intrinsic to the exceptional teams at LakefrontAI, this helps us unravel complexity and solve some of the toughest problems out there. An ownership mindset in all we do, a future-focused approach to solving problems, bringing the breadth and depth of expertise, and daring to experiment with unconventional methods to ultimately deliver value.</h2>
-          </div>
-          <div>
-            <button className="bg-white hover:bg-gray-700 border border-white-700 text-black py-3 px-10 rounded mb-5 ml-2">
-              See how we work
-            </button>
-            <button className="bg-white hover:bg-gray-700 border border-white-700 text-black py-3 px-10 rounded mb-5 ml-2">
-              See how we work
-            </button>
-          </div>
-        </div>
-        <div className='mr-1 mt-20 ml-1'>
-          <Image
-            src="/aiblock.jpeg"
-            alt="Logo"
-            width={"600"}
-            height={"300"}
-            className="relative "
-          />
-        </div>
-      </div>
-      {/* tiger gensection */}
-
+      <FeatureSection />
       <Services />
 
       <section className="container mx-auto p-10 md:py-0 px-0 md:p-0 md:px-0">

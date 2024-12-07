ebbc09894af73d03b94a7febe393640f111b3815
anjulk <anjulkumar2001@gmail.com>
2024-12-07 16:26:37 -0500

    hero changes

--- app/components/Description.tsx
+++ app/components/Description.tsx
@@ -1,73 +1,68 @@
 import React from "react";
-import { images } from "./constants";
-import { motion } from "framer-motion";
-import Image from "next/image";
 
-type Props = {
-  activeImage: any;
-  clickNext: any;
-  clickPrev: any;
-};
 
-const Description = ({ activeImage, clickNext, clickPrev }: Props) => {
+
+const Description = () => {
+
   return (
-    <div className="gap-y-1 overflow-hidden text-xl leading-6 ">
-      
-      {images.map((elem, idx) => (
-        <div
-          key={idx}
-          className={`${
-            idx === activeImage
-              ? " w-full h-full  text-left"
-              : "hidden"
-          }`}
-        >
-          <motion.div
-            initial={{
-              opacity: idx === activeImage ? 0 : 0.5,
-              scale: idx === activeImage ? 1 : 0.3,
-            }}
-            animate={{
-              opacity: idx === activeImage ? 1 : 0.5,
-              scale: idx === activeImage ? 1 : 0.3,
-            }}
-            transition={{
-              ease: "linear",
-              duration: 2,
-              x: { duration: 1 },
-            }}
-            className=""
-          >
-            <div className="py-16 text-5xl font-extrabold text-white">{elem.title}</div>
-            <div className="leading-relaxed font-bold text-l tracking-wide italic text-white">
-              {" "}
-              {elem.desc}
-            </div>
-          </motion.div>
-
-          
-          <div className="absolute md:bottom-1 bottom-10 right-10 md:right-0 w-full flex justify-center items-center">
-            <div
-              className="absolute bottom-2 right-10 cursor-pointer"
-              onClick={clickPrev}
-            >
-              
-            </div>
-
-            <div
-              className="absolute bottom-2 right-2 cursor-pointer"
-              onClick={clickNext}
-            >
-              <Image src="/rights.svg"
-              width="20"
-              height="10"
-               alt="" />
-            </div>
-          </div>
-        </div>
-      ))}
+  <div className="bg-white">
+    <div className="relative isolate px-6 pt-14 lg:px-8">
+    <div
+      aria-hidden="true"
+      className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+    >
+      <div
+        style={{
+          clipPath:
+            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+        }}
+        className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+      />
     </div>
-    
+    <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+      <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+        <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+          Learn about our Next Gen AI product.{' '}
+          <a href="#" className="font-semibold text-indigo-600">
+            <span aria-hidden="true" className="absolute inset-0" />
+            Read more <span aria-hidden="true">&rarr;</span>
+          </a>
+        </div>
+      </div>
+      <div className="text-center">
+        <h1 className="text-balance text-2xl font-semibold tracking-tight text-gray-900 sm:text-2xl">
+        Unmatched Expertise to Elevate Your Business Success
+        </h1>
+        <p className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
+        Empowering your business with unrivaled expertise, innovative strategies, and transformative solutions to drive growth, unlock potential, and achieve lasting success
+        </p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <a
+            href="/signup"
+            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Get started
+          </a>
+          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+            Learn more <span aria-hidden="true">→</span>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div
+      aria-hidden="true"
+      className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+    >
+      <div
+        style={{
+          clipPath:
+            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+        }}
+        className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+      />
+    </div>
+  </div> 
+</div>   
   );
 };
 

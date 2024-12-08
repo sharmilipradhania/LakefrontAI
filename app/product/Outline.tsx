456d4e4f79c64a05f75daddfc8f7435666fca018
anjulk <anjulkumar2001@gmail.com>
2024-12-08 12:40:10 -0500

    add product

--- app/product/Outline.tsx
+++ app/product/Outline.tsx
@@ -19,8 +19,6 @@ const Outline = () => {
         router.push(`/dashboard/services/${formattedService}`);
 
         };
-
-        
   return (
     <main className="py-10">
     <div className="px-4 sm:px-6 lg:px-8">

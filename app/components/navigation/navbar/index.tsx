eaa2360f4e8c8a22abf521be0ceeb42e57f88d3b
anjulk <anjulkumar2001@gmail.com>
2024-12-10 14:46:47 -0500

    about us and blog page

--- app/components/navigation/navbar/index.tsx
+++ app/components/navigation/navbar/index.tsx
@@ -105,7 +105,7 @@ const Navbar = ({ toggle }: { toggle: () => void }) => {
               </li>
               {/* second */}
               <li className="hoverable hover:bg-gray-100 hover:text-white">
-                <a href="#" className="relative block  lg:p-3 lg:text-1xl hover:bg-gray-100 hover:text-red-900">Blog</a>
+                <a href="/blog" className="relative block  lg:p-3 lg:text-1xl hover:bg-gray-100 hover:text-red-900">Blog</a>
                 <div className="p-6 mega-menu mb-16 sm:mb-0 shadow-xl bg-gray-100">
                   <div className="container mx-auto w-full flex flex-wrap justify-between mx-2"> 
                   </div>
@@ -113,7 +113,7 @@ const Navbar = ({ toggle }: { toggle: () => void }) => {
               </li>
               {/* third */}
               <li className="hoverable hover:bg-gray-100 hover:text-white">
-                <a href="/blog" className="relative block  lg:p-3 lg:text-1xl hover:bg-gray-100 hover:text-red-900">About Us</a>
+                <a href="/aboutus" className="relative block  lg:p-3 lg:text-1xl hover:bg-gray-100 hover:text-red-900">About Us</a>
                
               </li>
               {/* forth */}

ff1d361dd4ca7e9cd2c6ee86a76d72359fd5eef1
anjulk <anjulkumar2001@gmail.com>
2024-12-28 12:45:59 -0500

    remove one fs

--- server/api/server.js
+++ server/api/server.js
@@ -12,7 +12,6 @@ const ini = require("ini");
 const { OpenAI } = require("openai");
 const { Configuration, OpenAIApi } = require('openai');
 const multer = require('multer');
-const fs = require('fs');
 require("dotenv").config();
 
 const { callOpenAI, callGemini } = require("./apiUtils");

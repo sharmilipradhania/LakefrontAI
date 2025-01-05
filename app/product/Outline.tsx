51d36ec07f66d189cf0b258064d6007efeebb0f4
anjulk <anjulkumar2001@gmail.com>
2025-01-04 23:08:04 -0500

    ask anything

--- app/product/Outline.tsx
+++ app/product/Outline.tsx
@@ -5,7 +5,7 @@ import React, { useEffect, useState } from 'react';
 const lakefrontaiServices = [
   "Model Training",
   "Document Summary",
-  "Data Catalog",
+  "Ask Anything",
   "AI Agent",
 ];
 

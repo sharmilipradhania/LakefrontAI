a63bd2737410a7bf5340deacfe25107672987f26
Anjul Kumar <anjulkumar@Anjuls-MacBook-Pro.local>
2025-02-21 11:14:04 -0500

    response

--- server/api/routes/slackRoutes.js
+++ server/api/routes/slackRoutes.js
@@ -150,9 +150,6 @@ router.post("/slack/interactions", async (req, res) => {
     // ✅ Extract Slack data from request
     const {  response_url } = req.body; // Extract Slack's response_url
 
-    if (!response_url) {
-      return res.status(400).json({ error: "Invalid Slack request: response_url missing" });
-    }
     console.log("✅ Response Url:", response_url);
     if (payload.type === "view_submission") {
       const selectedExperiment = payload.view.state.values.experiment_select.selected_option.value;
